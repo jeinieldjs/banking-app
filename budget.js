1c327ef37221b9aca3e74fec624f2a74a1132577
jeinieldjs <silvestrejeiniel@gmail.com>
2023-08-24 15:22:48 +0800

    Updates add expense functionality, saves expenses to local storage

--- budget.js
+++ budget.js
@@ -1,95 +1,120 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let logoutBtn = document.getElementById("logout-btn");
-  logoutBtn.addEventListener("click", logoutUser);
+let logoutBtn = document.getElementById("logout-btn");
+logoutBtn.addEventListener("click", logoutUser);
 
-  function logoutUser() {
+function logoutUser() {
     localStorage.removeItem("loggedInUser");
     window.location.href = "index.html";
     alert("Logout Successfully!");
+}
+
+let userDisplay = document.getElementById("greet-container");
+let loggedInUser = localStorage.getItem("loggedInUser");
+let clients = JSON.parse(localStorage.getItem("clients"));
+let loggedClient = clients.find((client) => client.name === loggedInUser);
+let balanceDisplay = document.getElementById("balance-amount");
+
+if (!loggedClient.expenses) {
+  loggedClient.expenses = [];
+}
+
+if (loggedInUser) {
+  let greet = document.createElement("h1");
+  greet.innerHTML = `WELCOME, <span id="usernameSpan">${loggedInUser}</span>!`;
+  userDisplay.appendChild(greet);
+
+  let usernameSpan = document.getElementById("usernameSpan");
+  usernameSpan.style.color = "#68A225";
+
+  let dateContainer = document.createElement("div");
+  dateContainer.setAttribute("class", "dash-containers");
+  userDisplay.appendChild(dateContainer);
+
+  function updateDateTime() {
+    let currentTime = new Date();
+    dateContainer.textContent = `${currentTime.toDateString()} | ${currentTime.toLocaleTimeString()}`;
   }
 
-  let userDisplay = document.getElementById("greet-container");
-  let loggedInUser = localStorage.getItem("loggedInUser");
+  updateDateTime();
+  setInterval(updateDateTime, 1000);
 
-  let loggedClient = clients.find((client) => client.name === loggedInUser);
-  let balanceDisplay = document.getElementById("balance-amount");
+    let expenses = loggedClient.expenses || [];
 
-  if (loggedInUser) {
-    let greet = document.createElement("h1");
-    greet.innerHTML = `WELCOME, <span id="usernameSpan">${loggedInUser}</span>!`;
-    userDisplay.appendChild(greet);
-
-    let usernameSpan = document.getElementById("usernameSpan");
-    usernameSpan.style.color = "#68A225";
-
-    let dateContainer = document.createElement("div");
-    dateContainer.setAttribute("class", "dash-containers");
-    userDisplay.appendChild(dateContainer);
-
-    function updateDateTime() {
-      let currentTime = new Date();
-      dateContainer.textContent = `${currentTime.toDateString()} | ${currentTime.toLocaleTimeString()}`;
-    }
-
-    updateDateTime();
-    setInterval(updateDateTime, 1000);
-
- 
-
-    if (loggedClient) {
-      let userBalance = loggedClient.balance.toFixed(2);
-      let formatBalance = parseFloat(userBalance).toLocaleString(undefined, {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      });
-      balanceDisplay.textContent = formatBalance;
-    }
-  }
+  displayExpenses(expenses);
+  displayBalance(loggedClient.balance);
 
   let addExpenseBtn = document.getElementById('add-expense-btn');
   addExpenseBtn.addEventListener('click',addExpense);
 
   function addExpense() {
-    let item = document.getElementById('item-input').value;
+    let item = document.getElementById('item-input').value.toUpperCase();
     let cost = parseFloat(document.getElementById('price-input').value);
     let timestamp = new Date().toLocaleString();
 
-    let tableBody = document.getElementById('expense-body');
-    let newRow = tableBody.insertRow();
+    let newExpense = {
+      item: item,
+      cost: cost,
+      timestamp: timestamp
+    };
 
-    let itemCell = newRow.insertCell(0);
-    let costCell = newRow.insertCell(1);
-    let timeCell = newRow.insertCell(2);
-    let deleteCell = newRow.insertCell(3);
-    
-    itemCell.textContent = item;
-    costCell.textContent = cost.toFixed(2);
-    deleteCell.innerHTML = '<i class="fas fa-trash delete-icon"></i>';
-    timeCell.textContent = timestamp;
-
+    expenses.push(newExpense);
+    loggedClient.expenses = expenses;
     loggedClient.balance -= cost;
-    let updatedBalance = loggedClient.balance.toFixed(2);
-    let formattedBalance = parseFloat(updatedBalance).toLocaleString(undefined, {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    });
-    balanceDisplay.textContent = formattedBalance; 
+    updateLocalStorage();
 
-    if (loggedClient.balance < 0){
-      balanceDisplay.style.color = 'red';
+    document.getElementById('item-input').value = '';
+    document.getElementById('price-input').value = '';
+
+    displayExpenses(expenses);
+    displayBalance(loggedClient.balance);
+  }
+
+  function updateLocalStorage() {
+    let clientIndex = clients.findIndex(client => client.name === loggedInUser);
+    if (clientIndex !== -1) {
+      clients[clientIndex] = loggedClient;
+      localStorage.setItem("clients", JSON.stringify(clients));
     }
   }
 
-  let deleteExp = document.querySelector('.delete-icon');
-  deleteExp.addEventListener('click', deleteExpense);
+  function displayExpenses(expenses) {
+    let tableBody = document.getElementById('expense-body');
+    tableBody.innerHTML = ''; 
 
-function deleteExpense(){
-  const confirmed = confirm('Are you sure you want to remove this expense item?');
-  if (confirmed){
-    tableBody.removeChild(newRow);
-    loggedClient.balance += cost;
+    expenses.forEach((expense,index) => {
+      let newRow = tableBody.insertRow();
+      newRow.insertCell(0).textContent = expense.item;
+      newRow.insertCell(1).textContent = expense.cost.toFixed(2);
+      newRow.insertCell(2).textContent = expense.timestamp;
+      newRow.insertCell(3).innerHTML = '<i class="fas fa-trash delete-icon"></i>';
+
+      let deleteIcon = newRow.querySelector('.delete-icon');
+      deleteIcon.addEventListener('click', () => deleteExpense(expense, index));
+    });
+  }
+
+  function displayBalance(balance) {
+    let formattedBalance = parseFloat(balance).toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
+  balanceDisplay.textContent = formattedBalance;
+
+    if (balance < 0){
+        balanceDisplay.style.color = 'red';
+      } else {
+        balanceDisplay.style.color = 'white';
+      }
+    }
+
+  function deleteExpense(expense, index) {
+    const confirmed = confirm('Are you sure you want to delete this expense?');
+    if (confirmed){
+      loggedClient.balance += expense.cost;
+      loggedClient.expenses.splice(index, 1);
+      updateLocalStorage();
+      displayExpenses(loggedClient.expenses);
+      displayBalance(loggedClient.balance);
+    }
   }
 }
-});
-
-//expenses can be added na, pero di pa nauupdate sa local storage yung balance and expenses ni client
--- dashboard.js
+++ dashboard.js
@@ -27,98 +27,95 @@
 //   : initialClients;
 
 // localStorage.setItem("clients", JSON.stringify(clients));
+let adminDisplay = document.getElementById("admin-dash");
+let loggedInUser = localStorage.getItem("loggedInUser");
 
-document.addEventListener("DOMContentLoaded", function () {
-  let adminDisplay = document.getElementById("admin-dash");
-  let loggedInUser = localStorage.getItem("loggedInUser");
+if (loggedInUser) {
+  let greet = document.createElement("h1");
+  greet.innerHTML = `WELCOME, <span id="usernameSpan">${loggedInUser}</span>!`;
+  adminDisplay.appendChild(greet);
 
-  if (loggedInUser) {
-    let greet = document.createElement("h1");
-    greet.innerHTML = `WELCOME, <span id="usernameSpan">${loggedInUser}</span>!`;
-    adminDisplay.appendChild(greet);
+  let usernameSpan = document.getElementById("usernameSpan");
+  usernameSpan.style.color = "#68A225";
 
-    let usernameSpan = document.getElementById("usernameSpan");
-    usernameSpan.style.color = "#68A225";
+  let dateContainer = document.createElement("div");
+  /*dateContainer.setAttribute("id", "date-container");*/
+  dateContainer.setAttribute("class", "dash-containers");
+  adminDisplay.appendChild(dateContainer);
 
-    let dateContainer = document.createElement("div");
-    /*dateContainer.setAttribute("id", "date-container");*/
-    dateContainer.setAttribute("class", "dash-containers");
-    adminDisplay.appendChild(dateContainer);
+  function updateDateTime() {
+    let currentTime = new Date();
+    dateContainer.textContent = `${currentTime.toDateString()} | ${currentTime.toLocaleTimeString()}`;
+  }
 
-    function updateDateTime() {
-      let currentTime = new Date();
-      dateContainer.textContent = `${currentTime.toDateString()} | ${currentTime.toLocaleTimeString()}`;
-    }
+  updateDateTime();
+  setInterval(updateDateTime, 1000);
 
-    updateDateTime();
-    setInterval(updateDateTime, 1000);
+  let todoContainer = document.createElement("div");
+  todoContainer.setAttribute("id", "todo-container");
+  adminDisplay.appendChild(todoContainer);
 
-    let todoContainer = document.createElement("div");
-    todoContainer.setAttribute("id", "todo-container");
-    adminDisplay.appendChild(todoContainer);
+  let taskInput = document.createElement("input");
+  taskInput.setAttribute("type", "text");
+  taskInput.setAttribute("id", "task-input");
+  taskInput.setAttribute("placeholder", "TODAY'S TASKS");
+  todoContainer.appendChild(taskInput);
 
-    let taskInput = document.createElement("input");
-    taskInput.setAttribute("type", "text");
-    taskInput.setAttribute("id", "task-input");
-    taskInput.setAttribute("placeholder", "TODAY'S TASKS");
-    todoContainer.appendChild(taskInput);
+  let taskList = document.createElement("ul");
+  todoContainer.appendChild(taskList);
 
-    let taskList = document.createElement("ul");
-    todoContainer.appendChild(taskList);
-
-    taskInput.addEventListener("keydown", function (event) {
-      if (event.key === "Enter" && taskInput.value.trim() !== "") {
-        addNewToDoItem(taskInput.value, false);
-        taskInput.value = "";
-        saveToDoItems();
+  taskInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter" && taskInput.value.trim() !== "") {
+      addNewToDoItem(taskInput.value, false);
+      taskInput.value = "";
+      saveToDoItems();
       }
+  });
+
+  function saveToDoItems() {
+    let items = [];
+    let listItems = taskList.querySelectorAll(".list-item");
+    listItems.forEach(function (listItem) {
+      let text = listItem.innerText;
+      let isDone = listItem.classList.contains("done");
+      items.push({ TEXT: text, DONE: isDone });
     });
 
-    function saveToDoItems() {
-      let items = [];
-      let listItems = taskList.querySelectorAll(".list-item");
-      listItems.forEach(function (listItem) {
-        let text = listItem.innerText;
-        let isDone = listItem.classList.contains("done");
-        items.push({ TEXT: text, DONE: isDone });
-      });
-
-      localStorage.setItem("toDoItems", JSON.stringify(items));
-    }
-
-    function addNewToDoItem(text, isDone) {
-      let listItem = document.createElement("li");
-      listItem.classList.add("list-item");
-      listItem.innerText = text;
-      if (isDone) {
-        listItem.classList.add("done");
-      }
-
-      listItem.addEventListener("click", function () {
-        listItem.classList.toggle("done");
-        saveToDoItems();
-      });
-
-      listItem.addEventListener("dblclick", function () {
-        taskList.removeChild(listItem);
-        saveToDoItems();
-      });
-
-      taskList.appendChild(listItem);
-    }
-
-    function loadToDoItems() {
-      let storedItems = JSON.parse(localStorage.getItem("toDoItems"));
-      if (storedItems) {
-        storedItems.forEach(function (item) {
-          addNewToDoItem(item.TEXT, item.DONE);
-        });
-      }
-    }
-
-    loadToDoItems();
+    localStorage.setItem("toDoItems", JSON.stringify(items));
   }
-});
+
+  function addNewToDoItem(text, isDone) {
+    let listItem = document.createElement("li");
+    listItem.classList.add("list-item");
+    listItem.innerText = text;
+    if (isDone) {
+      listItem.classList.add("done");
+    }
+
+    listItem.addEventListener("click", function () {
+      listItem.classList.toggle("done");
+      saveToDoItems();
+    });
+
+    listItem.addEventListener("dblclick", function () {
+      taskList.removeChild(listItem);
+      saveToDoItems();
+    });
+
+    taskList.appendChild(listItem);
+  }
+
+  function loadToDoItems() {
+    let storedItems = JSON.parse(localStorage.getItem("toDoItems"));
+    if (storedItems) {
+      storedItems.forEach(function (item) {
+      addNewToDoItem(item.TEXT, item.DONE);
+      });
+    }
+  }
+
+  loadToDoItems();
+}
 
 const currentDate = new Date();
 const currentDay = currentDate.getDate();
@@ -178,6 +175,4 @@ if (daysRemaining === 0) {
 }
 
 countdownDisplay.appendChild(countdownMessage);
-let adminDisplay = document.getElementById("admin-dash");
 adminDisplay.appendChild(countdownDisplay);
-;
